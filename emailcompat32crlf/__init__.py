160c8cd7c351b0bc38d8a8e0449f0adf92348b3d
Maurits van Rees <maurits@vanrees.org>
2024-05-31 23:38:24 +0200

    Fix AttributeError: 'Compat32' object attribute 'linesep' is read-only

--- emailcompat32crlf/__init__.py
+++ emailcompat32crlf/__init__.py
@@ -1,5 +1,11 @@
 import email
 
 
+# Patch the Compat32 class.
 email.policy.Compat32.linesep = "\r\n"
-email.policy.compat32.linesep = "\r\n"
+
+# Patch the compat32 instance.
+# You cannot directly set compat32.linesep.
+# The email package itself prevents this, but we can do it in the same way
+# that the email package does it.
+object.__setattr__(email.policy.compat32, "linesep", "\r\n")
